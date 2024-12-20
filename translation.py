434351b458a3294354eb189893bebf6c1b1a5f4d
AaNn05 <117485357+AaNn05@users.noreply.github.com>
2024-12-21 01:29:55 +0400

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,30 @@
+translations = {
+    "en": {
+        "title": "The Smart Choice For Future",
+        "subtitle": "Take the next step in your career with top-tier courses...",
+        "search_placeholder": "Search for a course...",
+        "all_categories": "All Categories",
+        "programming": "Programminng",
+        "business": "Business",
+        "design": "Design",
+        "continue": "Continue",
+        "buttons": {
+                    "log_in": "Log In",
+                    "sign_up": "Sign Up"
+                }
+    },
+    "hy": {
+        "title": "ԼԱՎԱԳՈՒՅՆ ԸՆՏՐՈՒԹՅՈՒՆԸ ՁԵՐ ԱՊԱԳԱՅԻ ՀԱՄԱՐ",
+        "subtitle": "Նախորդ քայլը դեպի ձեր կարիերան՝ բարձրորակ դասընթացներով...",
+        "search_placeholder": "Որոնել դասընթաց...",
+        "all_categories": "Բոլոր բաժինները",
+        "programming": "Ծրագրավորում",
+        "business": "Բիզնես",
+        "design": "Դիզայն",
+        "continue": "Շարունակել",
+        "buttons": {
+                    "log_in": "Մուտք գործել",
+                    "sign_up": "Գրանցվել"
+                }
+    },
+}
